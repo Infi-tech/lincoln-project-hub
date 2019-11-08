43218280f95cbb339eed1a57bb27a61a65b904f3
Infinical <inficalvin@gmail.com>
2019-11-08 17:39:24 +0300

    Pow config

--- priv/repo/migrations/20191108102851_create_users.exs
+++ priv/repo/migrations/20191108102851_create_users.exs
@@ -5,17 +5,17 @@ defmodule LincolnProjectHub.Repo.Migrations.CreateUsers do
     create table(:users) do
       add :email, :string, null: false
       add :password_hash, :string
-      add :first_name, :string, 
-      add :last_name, :string 
-      add :age, :string 
-      add :class, :string 
-      add :school, :string 
-      add :guardian_name, :string 
-      add :guardian_phone, :string 
-      add :guardian_email, :string 
-      add :emargency_name, :string 
-      add :emergency_phone, :string 
-      add :role, :string, default: "ambassador" 
+      add :first_name, :string
+      add :last_name, :string
+      add :age, :string
+      add :class, :string
+      add :school, :string
+      add :guardian_name, :string
+      add :guardian_phone, :string
+      add :guardian_email, :string
+      add :emargency_name, :string
+      add :emergency_phone, :string
+      add :role, :string, default: "ambassador"
 
       timestamps()
     end
